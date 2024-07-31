a7716dafa8986a36db7efc55cbebb4efc22b2b35
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2024-07-31 13:56:33 +0000

    paths fixed and remdered template linked

--- deployment/main.py
+++ deployment/main.py
@@ -1,15 +1,19 @@
-from flask import Flask,request
+from flask import Flask,request, jsonify, render_template
 import joblib
 import json
 import numpy as np
 
-model = joblib.load('/home/karam/Projects/AI/Personal/fpl/deployment/static/model.lgbm')
+model = joblib.load('/workspaces/fantasy-predictions/deployment/static/model.lgbm')
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
 def predict():
     print(request.data)
     data = np.array(json.loads(request.data.decode())['X']).reshape(1,-1)
     pd = list(model.predict(data))
     return {'prediction':list(pd)}
-app.run()
+app.run(debug=True)
