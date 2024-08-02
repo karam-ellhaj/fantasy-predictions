d347b959777fcdfcfd2b3ea54aef0e1a4365e220
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2024-08-02 04:56:44 +0000

    prediction functionality works with imputed missing features

--- deployment/main.py
+++ deployment/main.py
@@ -1,6 +1,5 @@
-from flask import Flask,request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 import joblib
-import json
 import numpy as np
 
 model = joblib.load('/workspaces/fantasy-predictions/deployment/static/model.lgbm')
@@ -10,10 +9,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    print(request.data)
-    data = np.array(json.loads(request.data.decode())['X']).reshape(1,-1)
-    pd = list(model.predict(data))
-    return {'prediction':list(pd)}
-app.run(debug=True)
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    try:
+        data = [float(request.form[key]) for key in request.form]
+        num_features_model = 21  
+        num_features_input = len(data) 
+        if num_features_input < num_features_model:
+            data.extend([0] * (num_features_model - num_features_input))
+        np_data = np.array(data).reshape(1, -1)
+        output = model.predict(np_data)
+        return jsonify({'prediction': output.tolist()})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
